5cbfef79322099ea51f5508bf05eb0d85eb84a45
Nick Louie <70557573+1-icenine@users.noreply.github.com>
2025-06-14 11:38:17 -0400

    Added a separate file for making SQL procedures

---
+++ SQL/IMDB/make_procedures.sql
@@ -0,0 +1,81 @@
+DROP PROCEDURE IF EXISTS GetTop10MoviesByGenre;
+DELIMITER //
+CREATE PROCEDURE GetTop10MoviesByGenre(IN genre_input VARCHAR(50))
+BEGIN
+    SELECT 
+        title,
+        releaseYear,
+        averageRating,
+        numVotes,
+        genre
+    FROM genre_split_temp
+    WHERE 
+        LOWER(genre) = LOWER(genre_input)
+        AND mediaType = 'movie'
+        AND numVotes >= 10000
+    ORDER BY averageRating DESC
+    LIMIT 10;
+END //
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS GetBottom10MoviesByGenre;
+DELIMITER //
+CREATE PROCEDURE GetBottom10MoviesByGenre(IN genre_input VARCHAR(50))
+BEGIN
+    SELECT 
+        title,
+        releaseYear,
+        averageRating,
+        numVotes,
+        genre
+    FROM genre_split_temp
+    WHERE 
+        LOWER(genre) = LOWER(genre_input)
+        AND mediaType = 'movie'
+        AND numVotes >= 10000
+    ORDER BY averageRating ASC
+    LIMIT 10;
+END //
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS GetTop10TVSeriesByGenre;
+DELIMITER //
+CREATE PROCEDURE GetTop10TVSeriesByGenre(IN genre_input VARCHAR(50))
+BEGIN
+    SELECT 
+		releaseYear,
+		title, 
+		mediaType, 
+		averageRating
+	FROM genre_split_temp
+	WHERE 
+		LOWER(genre) = LOWER(genre_input)
+		AND numVotes >= 10000
+		AND (mediaType = 'tvSeries' OR mediaType = 'tvMiniSeries')
+	ORDER BY averageRating DESC
+	LIMIT 10;
+END //
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS GetBottom10TVSeriesByGenre;
+DELIMITER //
+CREATE PROCEDURE GetBottom10TVSeriesByGenre(IN genre_input VARCHAR(50))
+BEGIN
+	SELECT 
+		releaseYear,
+		title, 
+		mediaType, 
+		averageRating
+	FROM genre_split_temp
+	WHERE 
+		LOWER(genre) = 'animation'
+		AND numVotes >= 10000
+		AND (mediaType = 'tvSeries' OR mediaType = 'tvMiniSeries')
+	ORDER BY averageRating ASC
+	LIMIT 10;
+END //
+DELIMITER ;
+
+
+
+
